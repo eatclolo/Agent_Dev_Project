c79ac9f70459080e122ba09e61bcea88dfb89659
Kevin Lo <eatclolo@gmail.com>
2026-04-26 03:34:19 +0900

    add: tool __init__ .py

---
+++ app/tools/__init__.py
@@ -0,0 +1,9 @@
+from app.tools.calculator_tool import calculator_tool_obj
+from app.tools.weather_tool import weather_tool_obj
+from app.tools.rag_tool import rag_tool_obj
+
+tools = [
+    calculator_tool_obj,
+    weather_tool_obj,
+    rag_tool_obj
+]
